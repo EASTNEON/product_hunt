4f4a54b06c6411f759c064bead9afd5dbd7e6096
Unknown <cjl7042@qq.com>
2019-01-10 16:05:16 +0800

    初步完成了APP发布页面
    完成了前段部分

--- products/views.py
+++ products/views.py
@@ -5,3 +5,10 @@ from django.shortcuts import render
 
 def products_list(request):
     return render(request, 'products_list.html')
+
+
+def publish(request):
+    if request.method == 'GET':
+       return render(request, 'publish.html')
+    elif request.method == 'POST':
+    	pass
