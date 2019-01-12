190a757e18a71b4aeead585080166677607688de
Unknown <cjl7042@qq.com>
2019-01-12 09:47:00 +0800

    完成

--- products/models.py
+++ products/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class Product(models.Model):
+    # 后台管理admin看到的内容
+    title = models.CharField(default='例：抖音—用短视频记录美好生活', max_length=50)
+    intro = models.TextField(default='在这里写APP介绍')
+    url   = models.CharField(default='Http://', max_length=100)
+    # default文件应首先从MEDIA_URL去找,选择的图片则会被放到uooad_to路径
+    icon  = models.ImageField(default='default_icon.png', upload_to='image/')
+    image = models.ImageField(default='default_image.png', upload_to='image/')
+
+    votes = models.IntegerField(default=1)
+    pub_date  = models.DateField()
+    # 绑定外键（一删都删）
+    Hunter = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+    def short_intro(self):
+        return self.intro[:60]
--- products/views.py
+++ products/views.py
@@ -1,14 +1,48 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect,get_object_or_404
+from django.contrib.auth.decorators import login_required
+from .models import Product
+from django.utils import timezone
 
 # Create your views here.
 
 
 def products_list(request):
-    return render(request, 'products_list.html')
+    products = Product.objects
+    return render(request, 'products_list.html', {'products': products})
 
 
+def detail(request, product_id):
+    product = get_object_or_404(Product, pk=product_id)
+    return render(request, 'detail.html', {'product': product})
+
+
+# 只有用户登录才执行，否则404
+@login_required
 def publish(request):
     if request.method == 'GET':
-       return render(request, 'publish.html')
+            return render(request, 'publish.html')
     elif request.method == 'POST':
-    	pass
+            title = request.POST['标题']
+            intro = request.POST['介绍']
+            url = request.POST['APP链接']
+
+            # 若request.POST为空则为None
+            # 但request.FILES为空则会报错，因此要try
+            try:
+                icon = request.FILES['APP图标']
+                image = request.FILES['大图']
+
+                product = Product()
+                product.title = title
+                product.intro = intro
+                product.url = url
+                product.icon = icon
+                product.image = image
+
+                # 获取当前时刻
+                product.pub_date = timezone.datetime.now()
+                product.Hunter = request.user
+                product.save()
+                return redirect('主页')
+            except Exception as e:
+                return render(request, 'publish.html', {'错误': '请上传图片！！！'})
